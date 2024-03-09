2bdc7ddfaa2d897ce4443dea5fd8c0b654284f72
Calin C <calin.chiper@tss-yonder.com>
2024-03-09 14:57:30 +0000

    Second one :D

---
+++ app.py
@@ -0,0 +1,13 @@
+
+
+import streamlit as st
+
+st.set_page_config(page_title="PII Analyzer and Anonymizer", page_icon="🔒")
+st.sidebar.success("Select a demo from the sidebar to get started.")
+
+st.title("PII Analyzer and Anonymizer")
+st.markdown(
+    """
+    TODO: Add a description of the app here.
+"""
+)
