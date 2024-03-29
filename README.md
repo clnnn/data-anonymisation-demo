8ed724ac65989764bb03c6470e43d9f57d02af7e
Calin C <calin.chiper@tss-yonder.com>
2024-03-29 14:47:29 +0000

    Add a description on the main page

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# data-anonymisation
+
+# PII Data Identification and Anonymization Demo
+
+This project is a demo for identifying and anonymizing PII data in a database 
+## Quickstart
+
+### DevContainer
+
+[![Open in Dev Containers](https://img.shields.io/static/v1?label=Dev%20Containers&message=Open&color=blue&logo=visualstudiocode)](https://vscode.dev/redirect?url=vscode://ms-vscode-remote.remote-containers/cloneInVolume?url=https://github.com/clnnn/letyca)
+
+### Running the app
+
+1. Just run the following command in the terminal:
+
+```bash
+streamlit run app.py
+```
--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-
-
 import streamlit as st
 
 st.set_page_config(page_title="PII Analyzer and Anonymizer", page_icon="🔒")
@@ -8,6 +6,26 @@ st.sidebar.success("Select a demo from the sidebar to get started.")
 st.title("PII Analyzer and Anonymizer")
 st.markdown(
     """
-    TODO: Add a description of the app here.
-"""
+            ## Intro
+            This app demonstrates how to identify and anonymize personally identifiable information (PII) from different types of sources.
+            
+            ## How to Use
+            Select a demo from the sidebar to get started. The demos include:
+            
+            #### Unstructured Text
+            Analyze and anonymize PII in unstructured text. In this demo, you can input text that contains PII or select a predefined example. You are able to choose from different anonymization methods.
+            
+            #### Images
+            Analyze and anonymize PII in images. In this demo, you can upload an image that contains PII and see the anonymized version.
+            
+            #### Live Video
+            Analyze and anonymize PII in live video. In this demo, you can use your webcam to capture live video. The video will blur faces in real-time.
+            
+            ## Technologies
+            - Python
+            - Streamlit
+            - Microsoft Presidio
+            - OpenCV
+            - MediaPipe
+            """
 )
